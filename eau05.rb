ce47fafb54f31146e095004c4386f5dd99e433a8
Symitsh <sandri822@hotmail.fr>
2022-03-28 09:07:53 +0200

    Ajout d'intitulé

--- eau05.rb
+++ eau05.rb
@@ -1,3 +1,4 @@
+# ***** String dans string *****
 # Créez un programme qui détermine si une chaîne de caractère se trouve dans une autre.
 
 # Fonction utilisée : ( Ne pas utiliser la méthode .include? )
