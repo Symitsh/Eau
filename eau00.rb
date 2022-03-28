064f52f93e8b4d48f4bb514a2e8c0c23dbb63530
Symitsh <sandri822@hotmail.fr>
2022-03-28 09:41:29 +0200

    Refactoring affichage

--- eau00.rb
+++ eau00.rb
@@ -1,7 +1,15 @@
 # ***** Combinaisons de 3 chiffres *****
 # Créez un programme qui affiche toutes les différentes combinaisons possibles de trois chiffres dans l’ordre croissant.
 
-# Fonction utilisée :
+# Fonctions utilisées :
+def print_result(tab)
+    while increment_is_true(tab)
+        print "#{tab[0]}#{tab[1]}#{tab[2]}"
+        (puts ; exit) if tab == [7, 8, 9]
+        print ", "
+    end
+end
+
 def increment_is_true(tab)
     if tab[2] < 9
         tab[2] = tab[2] + 1         
@@ -22,14 +30,11 @@ end
 (puts "error: wrong number of arguments"; exit) if ARGV.size != 0
 
 # Partie 2: Parsing
-a = 0
-b = 1
-c = 1
+a, b, c = 0, 1, 1
 tab = [a, b, c]
 
-# Partie 3 et 4: Résolution et Affichage
-while increment_is_true(tab)
-    print "#{tab[0]}#{tab[1]}#{tab[2]}"
-    (puts ; exit) if tab == [7, 8, 9]
-    print ", "
-end
+# Partie 3 Résolution
+solution = print_result(tab)
+
+# Partie 4: Affichage
+puts solution 
